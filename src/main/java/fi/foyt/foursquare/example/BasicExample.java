c44940d0bd363c9d08f2dc6b8541953d81f263a6
neilireson <n.ireson@sheffield.ac.uk>
2021-06-16 15:59:21 +0100

    Get categories example

--- src/main/java/fi/foyt/foursquare/example/BasicExample.java
+++ src/main/java/fi/foyt/foursquare/example/BasicExample.java
@@ -4,6 +4,7 @@ import fi.foyt.foursquare.api.FoursquareApi;
 import fi.foyt.foursquare.api.FoursquareApiException;
 import fi.foyt.foursquare.api.Result;
 import fi.foyt.foursquare.api.VenueSearchOptions;
+import fi.foyt.foursquare.api.entities.Category;
 import fi.foyt.foursquare.api.entities.CompactVenue;
 import fi.foyt.foursquare.api.entities.VenuesSearchResult;
 import picocli.CommandLine;
@@ -12,15 +13,13 @@ import picocli.CommandLine;
  * Basic search example
  *
  * @author rmangi
- * @TODO - more examples please :)
+ * @author n.ireson@sheffield.ac.uk
  */
 public class BasicExample {
 
-    public static void main(String[] args) {
-        VenueSearchOptions options = CommandLine.populateCommand(new VenueSearchOptions(), args);
 
-        // Initialize FoursquareApi.
-        FoursquareApi foursquareApi = new FoursquareApi();
+    private static void venueSearch(FoursquareApi foursquareApi, String[] args) {
+        VenueSearchOptions options = CommandLine.populateCommand(new VenueSearchOptions(), args);
 
         try {
             // After client has been initialized we can make queries.
@@ -32,11 +31,9 @@ public class BasicExample {
             if (result.getMeta().getCode() == 200) {
                 // if query was ok we can finally we do something with the data
                 for (CompactVenue venue : result.getResult().getVenues()) {
-                    // TODO: Do something we the data
                     System.out.println(venue);
                 }
             } else {
-                // TODO: Proper error handling
                 System.out.println("Error occurred: ");
                 System.out.println("  code: " + result.getMeta().getCode());
                 System.out.println("  type: " + result.getMeta().getErrorType());
@@ -45,5 +42,32 @@ public class BasicExample {
         } catch (FoursquareApiException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * see https://developer.foursquare.com/docs/build-with-foursquare/categories/
+     * @param foursquareApi
+     */
+    private static void getCategories(FoursquareApi foursquareApi) {
+
+        try {
+            Result<Category[]> results = foursquareApi.venuesCategories();
+            for (Category category : results.getResult()) {
+                System.out.println(category);
+            }
+
+        } catch (FoursquareApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Initialize FoursquareApi.
+        FoursquareApi foursquareApi = new FoursquareApi();
+
+//        venueSearch(foursquareApi, args);
+
+        getCategories(foursquareApi);
     }
 }
